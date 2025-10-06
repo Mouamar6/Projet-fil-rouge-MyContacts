fc3632b11034aa5df349210461e753faed3e33b5
Mouamar ADJAHO <mouamar.adjaho@epitech.eu>
2025-10-06 16:54:45 +0200

    Server add

--- server/server.js
+++ server/server.js
@@ -15,20 +15,17 @@ app.use(express.json());
 app.post('/auth/register', authController.register);
 app.post('/auth/login', authController.login);
 
-// Routes Contacts (protégées)
 app.get('/contacts', authMiddleware, contactController.getContacts);
 app.post('/contacts', authMiddleware, contactController.createContact);
 app.patch('/contacts/:id', authMiddleware, contactController.updateContact);
 app.delete('/contacts/:id', authMiddleware, contactController.deleteContact);
 
-// Swagger
+
 swaggerJsDocs(app);
 
-// MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connecté !'))
   .catch(err => console.log('Erreur MongoDB :', err));
 
-// Lancer serveur
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
