a4b68f91629c046445d00d9b6764cb9b7173e501
Mouamar ADJAHO <mouamar.adjaho@epitech.eu>
2025-10-06 16:20:26 +0200

    All error ajusted

--- server/models/Contact.js
+++ server/models/Contact.js
@@ -7,13 +7,5 @@ const contactSchema = new mongoose.Schema({
   phone: { type: String, required: true }
 }, { timestamps: true });
 
-const mongoose = require('mongoose');
-
-const contactSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  phone: { type: String, required: true }
-}, { timestamps: true });
 
 module.exports = mongoose.model('Contact', contactSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -6,13 +6,4 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 
-
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-}, { timestamps: true });
-
-
 module.exports = mongoose.model('User', userSchema);
