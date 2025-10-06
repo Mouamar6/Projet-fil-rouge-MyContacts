1c357257126a8df190837204a4da0edb7472c3e4
Mouamar6 <mouamaradjaho9@gmail.com>
2025-10-06 16:01:29 +0200

    Update Contact.js

--- server/models/Contact.js
+++ server/models/Contact.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
@@ -8,7 +7,6 @@ const contactSchema = new mongoose.Schema({
   phone: { type: String, required: true }
 }, { timestamps: true });
 
-=======
 const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
@@ -18,5 +16,4 @@ const contactSchema = new mongoose.Schema({
   phone: { type: String, required: true }
 }, { timestamps: true });
 
->>>>>>> 3d320c97d7f7d18783634c7e23d2c7b67b5e8aed
 module.exports = mongoose.model('Contact', contactSchema);
