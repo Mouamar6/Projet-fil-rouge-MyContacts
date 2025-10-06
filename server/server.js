3223e24f9875432f174ab26119ccd4805f7d7547
Mouamar ADJAHO <mouamar.adjaho@epitech.eu>
2025-10-06 15:56:04 +0200

    error review

--- server/server.js
+++ server/server.js
@@ -1,35 +1,3 @@
-
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-
-const authController = require('./controllers/authController');
-const contactController = require('./controllers/contactController');
-const authMiddleware = require('./middleware/authMiddleware');
-const swaggerJsDocs = require('./swagger');
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.post('/auth/register', authController.register);
-app.post('/auth/login', authController.login);
-
-// Routes Contacts (protégées)
-app.get('/contacts', authMiddleware, contactController.getContacts);
-app.post('/contacts', authMiddleware, contactController.createContact);
-app.patch('/contacts/:id', authMiddleware, contactController.updateContact);
-app.delete('/contacts/:id', authMiddleware, contactController.deleteContact);
-
-// Swagger
-swaggerJsDocs(app);
-
-// MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connecté !'))
-  .catch(err => console.log('Erreur MongoDB :', err));
-
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
